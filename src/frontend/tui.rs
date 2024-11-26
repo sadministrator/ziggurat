ae512a0188a61ce8cab48cf7fdd581c461367e04
sadministrator <marco@melproject.org>
2024-11-25 22:46:36 -0600

    small refactors

--- src/frontend/tui.rs
+++ src/frontend/tui.rs
@@ -1,21 +1,22 @@
-use std::io;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use std::{fmt, io};
 
-use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
-use crossterm::terminal::disable_raw_mode;
+use crossterm::{
+    event::{KeyCode, KeyEvent, KeyModifiers},
+    terminal::disable_raw_mode,
+};
 use eyre::Result;
-use tui::style::{Color, Modifier, Style};
-use tui::text::Span;
-use tui::widgets::ListState;
 use tui::{
     backend::Backend,
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, List, ListItem},
+    style::{Color, Modifier, Style},
+    text::Span,
+    widgets::{Block, Borders, List, ListItem, ListState},
     Terminal,
 };
 
-use super::cli;
+use super::cli::Provider;
 
 enum MenuOption {
     Providers,
@@ -25,9 +26,9 @@ enum MenuOption {
     Language,
 }
 
-enum Provider {
-    Google,
-    Llm,
+enum ProviderOption {
+    Add,
+    Entry(ProviderEntry),
 }
 
 impl MenuOption {
@@ -62,9 +63,30 @@ impl MenuOption {
     }
 }
 
+pub struct ProviderEntry {
+    name: String,
+    provider: Provider,
+}
+
+impl fmt::Display for ProviderEntry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match &self.provider {
+                Provider::GoogleTranslate { .. } =>
+                    format!("LLM Endpoint Credentials - {}", self.name),
+                Provider::Llm { .. } => format!("Google Credentials - {}", self.name),
+            }
+        )
+    }
+}
+
 pub struct AppState {
-    pub selected: MenuOption,
-    provider: Option<cli::Provider>,
+    selected_option: MenuOption,
+    selected_provider: ProviderOption,
+    confirmed_provider_idx: Option<usize>,
+    providers: Vec<ProviderEntry>,
     config_path: Option<PathBuf>,
     input_file: String,
     output_file: String,
@@ -74,8 +96,10 @@ pub struct AppState {
 impl AppState {
     pub fn new() -> Self {
         Self {
-            selected: MenuOption::Providers,
-            provider: None,
+            selected_option: MenuOption::Providers,
+            selected_provider: ProviderOption::Add,
+            confirmed_provider_idx: None,
+            providers: vec![],
             config_path: None,
             input_file: String::new(),
             output_file: String::new(),
@@ -105,51 +129,54 @@ where
         )
         .split(terminal.size().unwrap());
 
-    let normal_style = Style::default();
-    let highlighted_style = Style::default()
-        .bg(Color::Yellow)
-        .fg(Color::White)
-        .add_modifier(Modifier::BOLD);
-
     let state = &app_state.lock().unwrap();
 
     terminal.draw(|f| {
-        let provider_items = vec![
-            ListItem::new(Span::raw("Google Translate")),
-            ListItem::new(Span::raw("LLM")),
-        ];
-        let provider_list = List::new(provider_items)
-            .block(Block::default().title("Providers").borders(Borders::ALL))
-            .highlight_style(if state.selected.index() == 0 {
-                highlighted_style
-            } else {
-                normal_style
-            })
-            .highlight_symbol("> ");
+        let mut provider_items = vec!["Add provider".to_string()];
+
+        provider_items.extend(
+            state
+                .providers
+                .iter()
+                .map(|p| p.to_string())
+                .collect::<Vec<String>>(),
+        );
+
+        let provider_list = styled_list(
+            "Providers",
+            provider_items,
+            state.selected_option.index() == 0,
+        )
+        .highlight_style(
+            Style::default()
+                .bg(Color::Yellow)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol("> ");
 
         let mut provider_state = ListState::default();
-        provider_state.select(Some(state.selected.index()));
+        provider_state.select(Some(state.selected_option.index()));
 
-        let config_list = List::new(vec![list_item("None", state.selected.index() == 1)])
-            .block(Block::default().title("Config Path").borders(Borders::ALL));
-
-        let input_list = List::new(vec![list_item("None", state.selected.index() == 2)])
-            .block(Block::default().title("Input Path").borders(Borders::ALL));
-
-        let output_list = List::new(vec![list_item("None", state.selected.index() == 3)])
-            .block(Block::default().title("Output Path").borders(Borders::ALL));
-
-        let language_list = List::new(vec![ListItem::new("None")])
-            .block(
-                Block::default()
-                    .title("Language Code")
-                    .borders(Borders::ALL),
-            )
-            .style(if state.selected.index() == 4 {
-                highlighted_style
-            } else {
-                normal_style
-            });
+        let config_list = styled_list(
+            "Config Path",
+            vec!["None".to_string()],
+            state.selected_option.index() == 1,
+        );
+        let input_list = styled_list(
+            "Input Path",
+            vec!["None".to_string()],
+            state.selected_option.index() == 2,
+        );
+        let output_list = styled_list(
+            "Output Path",
+            vec!["None".to_string()],
+            state.selected_option.index() == 3,
+        );
+        let language_list = styled_list(
+            "Language Code",
+            vec!["None".to_string()],
+            state.selected_option.index() == 4,
+        );
 
         f.render_stateful_widget(provider_list, chunks[0], &mut provider_state);
         f.render_widget(config_list, chunks[1]);
@@ -168,22 +195,40 @@ pub fn handle_event(key: KeyEvent, app_state: Arc<Mutex<AppState>>) -> Result<()
         KeyEvent {
             code: KeyCode::Up, ..
         } => {
-            state.selected = state.selected.previous();
+            state.selected_option = state.selected_option.previous();
         }
         KeyEvent {
             code: KeyCode::Down,
             ..
         } => {
-            state.selected = state.selected.next();
+            state.selected_option = state.selected_option.next();
         }
         KeyEvent {
             code: KeyCode::Enter,
             ..
         } => {
+            match state.selected_option {
+                MenuOption::Providers => {
+                    match &state.selected_provider {
+                        ProviderOption::Add => {
+                            // go to menu with list: "Google Cloud Credentials", "OpenAI-compatible API Credentials"
+                            todo!()
+                        }
+                        ProviderOption::Entry(ProviderEntry { name, provider }) => {
+                            todo!()
+                        }
+                    };
+                }
+                MenuOption::Config => todo!(),
+                MenuOption::Input => todo!(),
+                MenuOption::Output => todo!(),
+                MenuOption::Language => todo!(),
+            };
+
             let mut buffer = String::new();
             io::stdin().read_line(&mut buffer)?;
 
-            match state.selected {
+            match state.selected_option {
                 MenuOption::Providers => todo!(),
                 MenuOption::Config => todo!(),
                 MenuOption::Input => todo!(),
@@ -214,15 +259,21 @@ pub fn handle_event(key: KeyEvent, app_state: Arc<Mutex<AppState>>) -> Result<()
     Ok(())
 }
 
-fn list_item(content: &str, is_selected: bool) -> ListItem {
+fn styled_list(title: &str, items: Vec<String>, is_selected: bool) -> List {
     let style = if is_selected {
         Style::default()
             .bg(Color::Yellow)
-            .fg(Color::Black)
             .add_modifier(Modifier::BOLD)
     } else {
         Style::default()
     };
 
-    ListItem::new(Span::styled(content, style))
+    let items: Vec<ListItem> = items
+        .into_iter()
+        .map(|item| ListItem::new(Span::styled(item, Style::default())))
+        .collect();
+
+    List::new(items)
+        .block(Block::default().title(title).borders(Borders::ALL))
+        .style(style)
 }
